0bf8912dd97efd997a9f2d74c4b7257b9d250852
Osiris <potterosiris@gmail.com>
2021-03-15 13:12:27 -0400

    schema

--- portfolio-server/models/Gavin.js
+++ portfolio-server/models/Gavin.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 
-const gavinSchema = {
+const gavinSchema = Schema({
     username: { type: String, required: true },
     password: { type: String, required: true }
-}
+})
 
 module.exports = mongoose.model("Gavin", gavinSchema)
