3bd6431ebd3b0403fde5c7e8498cb04d12468403
Osiris <potterosiris@gmail.com>
2021-03-15 16:40:12 -0400

    submit a question

--- portfolio-server/routes/questions-routes.js
+++ portfolio-server/routes/questions-routes.js
@@ -5,5 +5,7 @@ const questionController = require("../controllers/question-controller")
 
 const router = express.Router()
 
+router.post("/submitquestion", questionController.submitAQuestion)
+
 
 module.exports = router
