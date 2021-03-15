a3a28e512f93eefaac74859843898b0fbac842c7
Osiris <potterosiris@gmail.com>
2021-03-15 16:30:40 -0400

    schema

--- portfolio-server/models/AnonymousQuestion.js
+++ portfolio-server/models/AnonymousQuestion.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const questionSchema = new Schema({
+    question: { type: String, required: true },
+    gavin: { type: mongoose.Types.ObjectId, required: true, ref: "Gavin" }
+})
