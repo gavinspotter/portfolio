beb44ede8816a8aa4d68a2899739369dccb3a21f
Osiris <potterosiris@gmail.com>
2021-03-15 13:14:47 -0400

    new schema

--- portfolio-server/models/Gavin.js
+++ portfolio-server/models/Gavin.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 
-const gavinSchema = Schema({
+const gavinSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true }
 })
