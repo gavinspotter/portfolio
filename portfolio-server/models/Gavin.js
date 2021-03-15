b9c3527bf598e2ab68868d9c53115f1178850f8d
Osiris <potterosiris@gmail.com>
2021-03-14 21:05:50 -0400

    gavin Schema

--- portfolio-server/models/Gavin.js
+++ portfolio-server/models/Gavin.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+
+const gavinSchema = {
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+}
