bac4b8fb884f2d4e7f4bb24beb3781eddf55dfcd
Osiris <potterosiris@gmail.com>
2021-03-15 16:41:50 -0400

    gavin schema

--- portfolio-server/models/Gavin.js
+++ portfolio-server/models/Gavin.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema
 
 const gavinSchema = new Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    questions: [{ type: mongoose.Types.ObjectId, required: true, ref: "Question" }]
 })
 
 module.exports = mongoose.model("Gavin", gavinSchema)
