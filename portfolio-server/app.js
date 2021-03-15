03146b5a4a72ce6c122b16cdb2c1f4e378e26b96
Osiris <potterosiris@gmail.com>
2021-03-14 20:58:47 -0400

    mw

--- portfolio-server/app.js
+++ portfolio-server/app.js
@@ -12,3 +12,11 @@ app.use((req, res, next) => {
     const error = new HttpError("could not find this route", 404);
     throw error;
 });
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || "an unknown error occured" });
+});
