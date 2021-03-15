386555d67024ba9667bc103ce6dd1d90463db2dd
Osiris <potterosiris@gmail.com>
2021-03-15 13:04:21 -0400

    post

--- portfolio-server/routes/gavin-routes.js
+++ portfolio-server/routes/gavin-routes.js
@@ -3,3 +3,5 @@ const express = require("express")
 const gavinController = require("../controllers/gavin-controller")
 
 const router = express.Router()
+
+router.post("/signup", gavinController.signup)
