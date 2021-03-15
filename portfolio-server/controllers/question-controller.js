90ac6fadf97332a702a22881e4e6e97f7fb8df80
Osiris <potterosiris@gmail.com>
2021-03-15 16:35:12 -0400

    submit a question func

--- portfolio-server/controllers/question-controller.js
+++ portfolio-server/controllers/question-controller.js
@@ -3,3 +3,8 @@ const HttpError = require("../models/HttpError")
 const Question = require("../models/AnonymousQuestion")
 
 const Gavin = require("../models/Gavin")
+
+
+const submitAQuestion = async (req, res, next) => {
+
+} 
