1216e72eb79e5bfcee0ab70f2e3ccbc4e8536da4
Osiris <potterosiris@gmail.com>
2021-03-16 19:54:43 -0400

    repost schema

--- portfolio-server/models/Reposts.js
+++ portfolio-server/models/Reposts.js
@@ -2,3 +2,9 @@ const mongoose = require("mongoose")
 
 
 const Schema = mongoose.Schema
+
+const repostSchema = new Schema({
+    question: { type: mongoose.Types.ObjectId, required: true, ref: "Question" },
+    answer: { type: String, required: true },
+    gavin: { type: mongoose.Types.ObjectId, required: true, ref: "Gavin" }
+})
