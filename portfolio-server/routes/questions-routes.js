9b59194b84c948ac9dae576d9ee210008501ae9f
Osiris <potterosiris@gmail.com>
2021-03-15 21:23:07 -0400

    check auth import

--- portfolio-server/routes/questions-routes.js
+++ portfolio-server/routes/questions-routes.js
@@ -3,7 +3,12 @@ const express = require("express")
 const questionController = require("../controllers/question-controller")
 
 
+
 const router = express.Router()
+
+const checkAuth = require('../middleware/check-auth');
+
+
 
 router.post("/submitquestion", questionController.submitAQuestion)
 
