e46fdb0e45488c4022d5814421e470ec0c146a5d
Osiris <potterosiris@gmail.com>
2021-03-16 18:26:54 -0400

    repost another question

--- portfolio-server/controllers/question-controller.js
+++ portfolio-server/controllers/question-controller.js
@@ -74,5 +74,9 @@ const getQuestions = async (req, res, next) => {
 
 }
 
+const repostAQuestion = (req, res, next) => {
+
+}
+
 exports.getQuestions = getQuestions
 exports.submitAQuestion = submitAQuestion
