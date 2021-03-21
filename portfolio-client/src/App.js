2359f96e7266e4ee7fd3bb3c403af0aff9507bd9
Osiris <potterosiris@gmail.com>
2021-03-20 20:27:34 -0400

    auth context

--- portfolio-client/src/App.js
+++ portfolio-client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom"
 
 
+import { AuthContext } from "./shared/context/auth-context"
+import { useAuth } from './shared/hooks/auth-hook';
+
 const App = () => {
   return (
     <div>
