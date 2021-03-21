0eeb70484e5090a396c764c340211d905df5143d
Osiris <potterosiris@gmail.com>
2021-03-20 20:24:11 -0400

    app js

--- portfolio-client/src/gavin/pages/Signin.js
+++ portfolio-client/src/gavin/pages/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Signup = () => {
+const Signin = () => {
 
     return (
         <div>
@@ -9,4 +9,4 @@ const Signup = () => {
     )
 }
 
-export default Signup
+export default Signin
