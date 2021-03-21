cb0c19b39e8d5a74485f4314d5b1d842d0105939
Osiris <potterosiris@gmail.com>
2021-03-20 20:05:15 -0400

    button

--- portfolio-client/src/shared/components/FormElements/Button.js
+++ portfolio-client/src/shared/components/FormElements/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Buttton = () => {
+const Button = () => {
     return (
         <div>
 
@@ -8,4 +8,4 @@ const Buttton = () => {
     )
 }
 
-export default Buttton
+export default Button
