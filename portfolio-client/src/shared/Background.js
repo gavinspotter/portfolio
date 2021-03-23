03882716372681107a24b6c0200c5d978b42d0b9
Osiris <potterosiris@gmail.com>
2021-03-23 10:38:36 -0400

    background

--- portfolio-client/src/shared/Background.js
+++ portfolio-client/src/shared/Background.js
@@ -5,9 +5,9 @@ import "../css/style.css"
 const Background = (props) => {
     return (
         <div className="background__cover">
-            <div className="background__slide">
-                {props.children}
-            </div>
+
+            {props.children}
+
 
         </div>
     )
