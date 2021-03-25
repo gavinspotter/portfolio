8df97bae91522562a6b015ec18f36f7fc77f5711
Osiris <potterosiris@gmail.com>
2021-03-24 20:06:46 -0400

    use effect

--- portfolio-client/src/gavin/pages/Questions.js
+++ portfolio-client/src/gavin/pages/Questions.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
 const Questions = () => {
 
+    useEffect(() => { }, [])
 
     return (
         <div>
