669f89e27fa7e432c0f24c09cfeb6de36099ca1e
Osiris <potterosiris@gmail.com>
2021-03-25 21:53:49 -0400

    use state

--- portfolio-client/src/gavin/pages/Questions.js
+++ portfolio-client/src/gavin/pages/Questions.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 
 import { AuthContext } from "../../shared/context/auth-context"
