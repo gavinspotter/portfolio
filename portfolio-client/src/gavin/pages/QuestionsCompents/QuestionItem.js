51986f093a815eb5752ba458268d6beef63e60c2
Osiris <potterosiris@gmail.com>
2021-03-30 18:33:00 -0400

    form

--- portfolio-client/src/gavin/pages/QuestionsCompents/QuestionItem.js
+++ portfolio-client/src/gavin/pages/QuestionsCompents/QuestionItem.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 import "../../../css/style.css"
+import Input from '../../../shared/components/FormElements/Input'
 
 const QuestionItem = (props) => {
     return (
         <div className="questions__card">
+            <div className="questions__card--text">
+                {props.question}
 
-            {props.question}
+            </div>
+
+            <div>
+                <form>
+                    <Input />
+
+                </form>
+
+            </div>
         </div>
     )
 }
