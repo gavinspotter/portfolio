3503f1831c7b30df9b75ee23bd55c741c27f822a
Osiris <potterosiris@gmail.com>
2021-03-30 19:19:47 -0400

    questions

--- portfolio-server/models/AnonymousQuestion.js
+++ portfolio-server/models/AnonymousQuestion.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 const questionSchema = new Schema({
     question: { type: String, required: true },
     gavin: { type: mongoose.Types.ObjectId, required: true, ref: "Gavin" },
-    repost: { type: mongoose.Types.ObjectId, required: true, ref: "Repost" }
+    repost: { type: mongoose.Types.ObjectId, ref: "Repost" }
 })
 
 module.exports = mongoose.model("Question", questionSchema)
