5ed2a37a799071217ea553d9e3a06244bf5bf3b1
Osiris <potterosiris@gmail.com>
2021-03-30 18:36:15 -0400

    use http clinet

--- portfolio-client/src/gavin/pages/QuestionsCompents/QuestionItem.js
+++ portfolio-client/src/gavin/pages/QuestionsCompents/QuestionItem.js
@@ -3,8 +3,12 @@ import React from 'react'
 import "../../../css/style.css"
 import Button from '../../../shared/components/FormElements/Button'
 import Input from '../../../shared/components/FormElements/Input'
+import { useHttpClient } from "../../../shared/hooks/http-hook"
 
 const QuestionItem = (props) => {
+
+
+
     return (
         <div className="questions__card">
             <div className="questions__card--text">
